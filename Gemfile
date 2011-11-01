908767717e2b33c09fe50a5e0e62593aca96f428
Rob Cameron <cannikinn@gmail.com>
2011-11-01 16:37:56 -0700

    Added code for gem

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in format_fallback.gemspec
 gemspec
+
+gem 'actionpack', '~>3.0.10'
--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -1,5 +1,20 @@
 require "format_fallback/version"
 
-module FormatFallback
-  # Your code goes here...
+module ActionView
+  class PathSet < Array
+
+    def find(path, prefix = nil, partial = false, details = {}, key = nil)
+      begin
+        template = find_all(path, prefix, partial, details, key).first
+        raise MissingTemplate.new(self, "#{prefix}/#{path}", details, partial) unless template
+      rescue MissingTemplate => e
+        raise e if details[:formats] == [:html]
+        html_details = details.dup.merge(:formats => [:html])
+        template = find_all(path, prefix, partial, html_details, key).first
+        raise MissingTemplate.new(self, "#{prefix}/#{path}", details, partial) unless template
+      end
+      template
+    end
+
+  end
 end
