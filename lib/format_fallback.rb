5787289072ec08929378ea4f230427140c7976c4
Rob Cameron <cannikinn@gmail.com>
2011-11-01 17:37:37 -0700

    Using alias_method_chain

--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -3,18 +3,19 @@ require "format_fallback/version"
 module ActionView
   class PathSet < Array
 
-    def find(path, prefix = nil, partial = false, details = {}, key = nil)
+    def find_with_default_template(path, prefix = nil, partial = false, details = {}, key = nil)
       begin
-        template = find_all(path, prefix, partial, details, key).first
-        raise MissingTemplate.new(self, "#{prefix}/#{path}", details, partial) unless template
+        find_without_default_template(path, prefix, partial, details, key)
       rescue MissingTemplate => e
         raise e if details[:formats] == [:html]
         html_details = details.dup.merge(:formats => [:html])
-        template = find_all(path, prefix, partial, html_details, key).first
-        raise MissingTemplate.new(self, "#{prefix}/#{path}", details, partial) unless template
+        find_without_default_template(path, prefix, partial, html_details, key)
+        #template = find_all(path, prefix, partial, html_details, key).first
+        #raise MissingTemplate.new(self, "#{prefix}/#{path}", details, partial) unless template
       end
-      template
+      #template
     end
+    alias_method_chain :find, :default_template
 
   end
 end
--- lib/format_fallback/version.rb
+++ lib/format_fallback/version.rb
@@ -1,3 +1,3 @@
 module FormatFallback
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
