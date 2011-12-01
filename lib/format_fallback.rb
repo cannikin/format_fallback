91aba381fd290346aff1e9993c6c699011345542
Alessandro Morandi <alessandro@headshift.com>
2011-12-01 16:51:51 +0000

    Fix: do not fallback when a layout is involved. Layout fallbacks are managed in their own way by ActionView.

--- lib/format_fallback.rb
+++ lib/format_fallback.rb
@@ -4,12 +4,17 @@ module ActionView
   class PathSet < Array
 
     def find_with_default_template(path, prefix = nil, partial = false, details = {}, key = nil)
-      begin
+      if prefix == "layouts"
+        # Layouts have their own way of managing fallback, better leave them alone
         find_without_default_template(path, prefix, partial, details, key)
-      rescue MissingTemplate => e
-        raise e if details[:formats] == [:html]
-        html_details = details.dup.merge(:formats => [:html])
-        find_without_default_template(path, prefix, partial, html_details, key)
+      else
+        begin
+          find_without_default_template(path, prefix, partial, details, key)
+        rescue MissingTemplate => e
+          raise e if details[:formats] == [:html]
+          html_details = details.dup.merge(:formats => [:html])
+          find_without_default_template(path, prefix, partial, html_details, key)
+        end
       end
     end
     alias_method_chain :find, :default_template
